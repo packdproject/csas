5cf5e11ecbbae58e3c6affb39db815a2c40536a8
packdproject <connecttodell@gmail.com>
2026-06-14 09:59:08 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,6 @@
-const API_URL =
-"https://script.google.com/macros/s/AKfycbwKJUJieEWG91Bu_FQsgJxV4FSvUTJCFbbByfuTh0FQk12eRk2WqZH4wLLiqYsXWMPL/exec";
+// Konfigurasi API
+const CONFIG = {
+    // Ganti dengan URL Apps Script Anda (sudah sesuai)
+    BASE_URL: "https://script.google.com/macros/s/AKfycbwKJUJieEWG91Bu_FQsgJxV4FSvUTJCFbbByfuTh0FQk12eRk2WqZH4wLLiqYsXWMPL/exec",
+    TIMEOUT: 10000
+};
